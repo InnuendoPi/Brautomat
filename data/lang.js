0520f91d81a5853164c5a5f546e5417a8d304ee0
Innuendo <k.ader@pc-team.de>
2025-02-17 13:35:22 +0100

    V 1.49

--- data/lang.js
+++
@@ -1,7 +0,0 @@
-// language configuration
-const supportedLocales = ["de", "en"];  // language files
-
-// optional configuration 
-const defaultLocale = "de";         // dateTimePicker default language  (de, en, es, nl)
-const defaultTimeFormat = 'YMDHM';  // DateTimePicker Year Month Day Hour Minute
-const defaultFirstDayOfWeek = 1;    // DateTimePicker 0~6: Sunday~Saturday, e.g.: 1 (Monday), default: 0(Sunday)
